053bca7edcead98e4ca4332801bf605919b8c83b
dcant <diekey@qq.com>
2013-12-06 19:13:44 +0800

    add lab4

--- lock_smain.cc
+++ lock_smain.cc
@@ -2,7 +2,8 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include "lock_server.h"
+//#include "lock_server.h"
+#include "lock_server_cache.h"
 
 #include "jsl_log.h"
 
@@ -31,11 +32,21 @@ main(int argc, char *argv[])
   //jsl_set_debug(2);
 
 #ifndef RSM
-  lock_server ls;
+  /*lock_server ls;
   rpcs server(atoi(argv[1]), count);
   server.reg(lock_protocol::stat, &ls, &lock_server::stat);
   server.reg(lock_protocol::acquire, &ls, &lock_server::acquire);
-  server.reg(lock_protocol::release, &ls, &lock_server::release);
+<<<<<<< HEAD
+  server.reg(lock_protocol::release, &ls, &lock_server::release);*/
+
+  lock_server_cache ls;
+  rpcs server(atoi(argv[1]), count);
+  server.reg(lock_protocol::stat, &ls, &lock_server_cache::stat);
+  server.reg(lock_protocol::release, &ls, &lock_server_cache::release);
+  server.reg(lock_protocol::acquire, &ls, &lock_server_cache::acquire);
+
+//  server.reg(lock_protocol::release, &ls, &lock_server::release);
+
 #endif
 
 
